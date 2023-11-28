4edbb8b58c92acdb9a5d99d9ff51eefcd32b1146
Temel <mert_temel1999@outlook.com>
2023-11-28 18:20:50 +0300

    solid

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 import warnings
+import utils as helper
 warnings.simplefilter(action="ignore")
 # ss
 pd.set_option('display.max_columns', None)
@@ -18,42 +19,9 @@ pd.set_option('display.width', 500)
 
 data = pd.read_csv('../OkulProje/database/diabetes.csv')
 
-#Burada diabetes veri setini checkup ediyoruz.
-def check_df(dataframe, head=8):
-  print("##### Shape #####") #kac kolon kac satir
-  print(dataframe.shape)
-  print("##### Types #####") #kolonların, yani özelliklerin tipleri (int,float,str..)
-  print(dataframe.dtypes)
-  print("##### Tail #####") #Veri setinin son 5 degerini inceliyoruz.
-  print(dataframe.tail(head))
-  print("##### Head #####") #Veri setinin ilk 5 degerini inceliyoruz.
-  print(dataframe.head(head))
-  print("##### Null Analysis #####") #Bos deger olup olmadigini kontrol ediyoruz.
-  print(dataframe.isnull().sum())
-  print("##### Quantiles #####") #sayısal verilere sahip olan sütunların istatiksel değerlerine baktık.
-  #Hamilelik max 17 olarak girilmis, 17 kere hamilelik kulaga biraz imkansız geliyor. Verimizde Outlier degerler bulunuyor.
-  # Mesela glucose degeri min olarak 0 gosterilmis, glucose 0 olamaz. Demek ki null kısımlara 0 girilmis.
-  print(dataframe.describe([0,0.05, 0.50, 0.95, 0.99, 1]).T)
-
-check_df(data)
-
 # Kolon isimlerini büyültüyorum ki sorgulama vs yaparken yazmak, okumak kolay olsun.
 data.columns = [col.upper() for col in data.columns]
 data.head()
-
-#Outlier değerleri grafik üzerinde görebilmek için
-f, ax = plt.subplots(figsize=(20,20)) #f->figure and ax->axis
-fig = sns.boxplot(data=data, orient="h") #horizontally (grafiği yatayda alabilmek için)
-#plt.show()
-
-
-#Korelasyon analizi için
-#Mesela buradaki korelasyona bakarak, doğum ve yaş arasında 0.54 pozitif korelasyon var.
-#Outcome ı en çok etkileyen glikoz değeriymiş. 0.47
-#Glikoz değerinden sonra en çok etki eden BMI olmuş. 0.29
-#Yaş ile deri kalınlığı arasında da negatif korelasyon bulunmakta. -0.11
-sns.clustermap(data.corr(), annot = True, fmt = ".2f")
-#plt.show()
 
 
 # BASE MODEL KURULUMU
@@ -117,90 +85,35 @@ def plot_importance(model, features, num=len(X), save=False):
 
 plot_importance(rf_model, X)
 
+data = pd.read_csv('../OkulProje/database/diabetes.csv')
+
+helper.check_df(data)
+
+# Kolon isimlerini büyültüyorum ki sorgulama vs yaparken yazmak, okumak kolay olsun.
+data.columns = [col.upper() for col in data.columns]
+data.head()
+
+#Outlier değerleri grafik üzerinde görebilmek için
+f, ax = plt.subplots(figsize=(20,20)) #f->figure and ax->axis
+fig = sns.boxplot(data=data, orient="h") #horizontally (grafiği yatayda alabilmek için)
+#plt.show()
 
 
-#Degiskenleri kategorik, kardinal ve nümerik olarak ayırıyoruz.
-# Kardinal Degisken : kategorik degiskenin 20den fazla (buna biz karar veriyoruz 5 da  yazabilirdik.) sınıfı varsa kategorik gibi gorunen degiskenlerdir.
-"""
-Kardinal değişkenler, sayılarla ifade edilebilen ve belirli bir sıralamaya sahip olmayan değişkenlerdir. 
-Örneğin, bir ankette katılımcıların yaşları, eğitim seviyeleri veya gelir düzeyleri kardinal değişkenlere örnek olarak verilebilir.
-Aşağıda, katılımcıların yaşlarını içeren basit bir veri seti örneği bulunmaktadır:
+sns.clustermap(data.corr(), annot = True, fmt = ".2f")
+#plt.show()
 
-{25,30,35,40,22,28,32,37,45,29}
+cat_cols, num_cols, cat_but_car = helper.grab_col_names(data)
 
-Bu veri setindeki her bir sayı, bir katılımcının yaşı olarak temsil edilir. 
-Bu veri seti, kardinal bir değişkeni gösterir çünkü her bir değer sayısal olarak ifade edilebilir ve bu değerler arasında bir sıralama mevcuttur.
-"""
-def grab_col_names(dataframe, cat_th=10,
-                   car_th=20):  # essiz deger sayisi 10dan kucukse kategorik degisken, 5 den buyukse de kardinal degisken gibi dusunucez.
-  # Veri setimiz küçük olduğundan ben 5 ile sınırlandırdım.
-  cat_cols = [col for col in dataframe.columns if str(dataframe[col].dtypes) in ["category", "object", "bool"]]
-
-  num_but_cat = [col for col in dataframe.columns if
-                 dataframe[col].nunique() < cat_th and dataframe[col].dtypes in ["int", "float"]]
-
-  cat_but_car = [col for col in dataframe.columns if
-                 dataframe[col].nunique() > car_th and str(dataframe[col].dtypes) in ["category", "object"]]
-
-  cat_cols = num_but_cat + cat_cols
-  cat_cols = [col for col in cat_cols if col not in cat_but_car]
-
-  num_cols = [col for col in dataframe.columns if dataframe[col].dtypes in ["int", "float"]]
-  num_cols = [col for col in num_cols if col not in cat_cols]
-
-  print(f"Observations: {dataframe.shape[0]}")
-  print(f"Variables: {dataframe.shape[1]}")
-  print(f"Categorical Columns: {len(cat_cols)}")
-  print(f"Numerical Columns: {len(num_cols)}")
-  print(f"Categoric but Cardinal: {len(cat_but_car)}")
-  print(f"Numeric but Categoric: {len(num_but_cat)}")
-
-  return cat_cols, num_cols, cat_but_car
-
-cat_cols, num_cols, cat_but_car = grab_col_names(data)
-
-"""
-Observations: 768
-Variables: 9
-Categorical Columns: 1
-Numerical Columns: 8
-Categoric but Cardinal: 0
-Numeric but Categoric: 1
-(['OUTCOME'], ['PREGNANCIES', 'GLUCOSE', 'BLOODPRESSURE', 'SKINTHICKNESS', 'INSULIN', 'BMI', 'DIABETESPEDIGREEFUNCTION', 'AGE'], [])
-#Çıktıda aldığımız değerlere göre kardinal bir değişkenimiz bulunmamakta, 1 kategorik, 8 numerik, 1 tane de numerik ama kategorik (OUTCOME) değerimiz bulunmakta.
-"""
-
-#numerik değiskenler ve target analizi
-def target_summary_with_num(dataframe, target, numerical_col):
-    print(dataframe.groupby(target).agg({numerical_col: "mean"}), end="\n\n\n")
+#numerik kolonlarin birbiriyle iliskisi
+for col in num_cols:
+    helper.target_summary_with_num(data, "OUTCOME", col)
 
 for col in num_cols:
-    target_summary_with_num(data, "OUTCOME", col)
-
-#nümerik değişken analizi
-def num_summary(dataframe, numerical_col, plot=False):
-    quantiles = [0.05, 0.10, 0.20, 0.30, 0.40, 0.50, 0.60, 0.70, 0.80, 0.90, 0.95, 0.99]
-    print(dataframe[numerical_col].describe(quantiles).T)
-
-    if plot:
-        dataframe[numerical_col].hist(bins=20)
-        plt.xlabel(numerical_col)
-        plt.title(numerical_col)
-        #plt.show()
-
-for col in num_cols:
-    num_summary(data, col, plot=True)
+    helper.num_summary(data, col, plot=True)
 
 #kategorik degisken analizi yani sadece outcome
-def cat_summary(dataframe, col_name, plot=False):
-    print(pd.DataFrame({col_name: dataframe[col_name].value_counts(),
-                        "Ratio": 100 * dataframe[col_name].value_counts() / len(dataframe)}))
-    print("##########################################")
-    if plot:
-        sns.countplot(x=dataframe[col_name], data=dataframe)
-        #plt.show()
 
-cat_summary(data, "OUTCOME")
+helper.cat_summary(data, "OUTCOME")
 
 #0 değerlerini NaN olarak değiştirelim.
 nan_col=['GLUCOSE','BLOODPRESSURE','SKINTHICKNESS', 'INSULIN', 'BMI']
@@ -219,39 +132,13 @@ data.loc[data['OUTCOME'] == 0] = data.loc[data['OUTCOME'] == 0].fillna(median_va
 
 data.isnull().sum()
 
-#Aykırı değerimizi (Outlier) saptama işlemi için:
-def outlier_thresholds(dataframe, col_name, q1 = 0.25, q3 = 0.75):
-  quantile1 = dataframe[col_name].quantile(q1)
-  quantile3 = dataframe[col_name].quantile(q3)
-  interquantile_range = quantile3 - quantile1
-  up_limit = quantile3 + 1.5 * interquantile_range
-  low_limit = quantile1 - 1.5 * interquantile_range
-  return low_limit, up_limit
 
-#Thresholdlara göre outlier var mı yok mu diye kontrol etmek için:
-def check_outlier(dataframe, col_name):
-  low_limit, up_limit = outlier_thresholds(dataframe, col_name)
-  if dataframe[(dataframe[col_name] > up_limit) | (dataframe[col_name] < low_limit)].any(axis=None):
-    return True
-  else:
-    return False
-
-#Var olan outlierları görmek için:
-def grab_outliers(dataframe, col_name, index=False):
-  low, up = outlier_thresholds(dataframe, col_name)
-  if dataframe[((dataframe[col_name] < low) | (dataframe[col_name] > up))].shape[0] > 10:
-    print(dataframe[((dataframe[col_name] < low) | (dataframe[col_name] > up))].head())
-  else:
-    print(dataframe[((dataframe[col_name] < low) | (dataframe[col_name] > up))])
-  if index:
-    outlier_index = dataframe[((dataframe[col_name] < low) | (dataframe[col_name] > up))].index
-    return outlier_index
 
 for col in num_cols:
-  print(col,"için LOW LIMIT, UP LIMIT değerleri = ", outlier_thresholds(data,col),"\n")
+  print(col,"için LOW LIMIT, UP LIMIT değerleri = ", helper.outlier_thresholds(data,col),"\n")
 
 for col in num_cols:
-  print(col,"için outlier kontrolü", check_outlier(data,col),"\n")
+  print(col,"için outlier kontrolü", helper.check_outlier(data,col),"\n")
 
 
 #Bu çıktıdan alınan değerlere göre hamilelik (PREGNANCIES) 14-17 aralığındakiler outlier,
@@ -260,17 +147,13 @@ for col in num_cols:
 #BMI değerleri 0 olamaz null değerler var.
 #Deri kalınlığı min 0.6 mm, max 2.4mm dir. 0 olan değerler null değerdir.
 for col in num_cols:
-  print(col,"için var olan outlierlar\n", grab_outliers(data,col))
+  print(col,"için var olan outlierlar\n", helper.grab_outliers(data,col))
 
 #replace_with_thresholds fonksiyonunu uygulamadan önce yukarıda kararını verdiğimiz 0 değerleri null yapmalıyız ki onları da outlier olarak görmesin.
-def replace_with_thresholds(dataframe, variable):
-  low_limit, up_limit = outlier_thresholds(dataframe, variable)
-  dataframe.loc[(dataframe[variable] < low_limit), variable] = low_limit
-  dataframe.loc[(dataframe[variable] > up_limit), variable] = up_limit
 
 #şimdi kalan aykırı değerlerin değişimini sağlayabiliriz.
 for col in num_cols:
-  print(replace_with_thresholds(data, col))
+  print(helper.replace_with_thresholds(data, col))
 
 
 plt.hist(data, bins=10, edgecolor='black')  # 'bins' parametresiyle aralık sayısını belirleyebilirsiniz
@@ -330,7 +213,7 @@ data.loc[(data["BMI"] < 18.5) & ((data["SKINTHICKNESS"] > 2) & (data["SKINTHICKN
 data.loc[(data["BMI"] < 18.5) & ((data["SKINTHICKNESS"] > 2.5) & (data["SKINTHICKNESS"] <= 3.25)), "NEW_BMI_SKIN_THIC_NOM"] = "UnderweightThick"
 data.loc[((data["BMI"] >= 18.5) & (data["BMI"] < 25)) & ((data["SKINTHICKNESS"] > 1.25) & (data["SKINTHICKNESS"] <= 2)), "NEW_BMI_SKIN_THIC_NOM"] = "HealthyThin"
 data.loc[((data["BMI"] >= 18.5) & (data["BMI"] < 25)) & ((data["SKINTHICKNESS"] > 2) & (data["SKINTHICKNESS"] <= 2.5)), "NEW_BMI_SKIN_THIC_NOM"] = "Healthy_Healthy"
-#data.loc[((data["BMI"] >= 18.5) % (data["BMI"] < 25)) & ((data["SKINTHICKNESS"] > 2 & (data["SKINTHICKNESS"] <= 2.5)), "NEW_BMI_SKIN_THIC_NOM"] = "Healthy_Healthy"
+#data.loc[((data["BMI"] >= 18.5) % (data["BMI"] < 25)) & ((data["SKINTHICKNESS"] > 2) & (data["SKINTHICKNESS"] <= 2.5)), "NEW_BMI_SKIN_THIC_NOM"] = "Healthy_Healthy"
 data.loc[((data["BMI"] >= 18.5)) & (data["BMI"] < 25) & ((data["SKINTHICKNESS"] > 2.5) & (data["SKINTHICKNESS"] <= 3.25)), "NEW_BMI_SKIN_THIC_NOM"] = "HealthyThick"
 data.loc[((data["BMI"] >= 25) % (data["BMI"] < 30)) & ((data["SKINTHICKNESS"] > 1.25) & (data["SKINTHICKNESS"] <= 2)), "NEW_BMI_SKIN_THIC_NOM"] = "OverweightThin"
 data.loc[((data["BMI"] >= 25) % (data["BMI"] < 30)) & ((data["SKINTHICKNESS"] > 2) & (data["SKINTHICKNESS"] <= 2.5)), "NEW_BMI_SKIN_THIC_NOM"] = "OverweightHealthy"
@@ -360,7 +243,7 @@ def set_insulin(dataframe, col_name="INSULIN"):
 # ENCODING
 
 # Değişkenlerin tiplerine göre ayrılması işlemi
-cat_cols, num_cols, cat_but_car = grab_col_names(data)
+cat_cols, num_cols, cat_but_car = helper.grab_col_names(data)
 
 # LABEL ENCODING
 def label_encoder(dataframe, binary_col):
@@ -368,9 +251,9 @@ def label_encoder(dataframe, binary_col):
     dataframe[binary_col] = labelencoder.fit_transform(dataframe[binary_col])
     return dataframe
 
-binary_cols = [col for col in data.columns if data[col].dtypes == "O" and data[col].nunique() == 2]
+binary_cols = [col for col in data.columns if data[col].dtypes == "O" and data[col].nunique() == 2 ]
 binary_cols
-
+data.head(20)
 for col in binary_cols:
     data = label_encoder(data, col)
 
